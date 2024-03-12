c536ba27afb7b36da808a53aae89f323423f3ed0
Дамба Манджиев <mdamba2020@yandex.ru>
2024-03-12 07:33:05 +0300

    add exercise_1

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,17 +1,18 @@
-package ru.netology;
+import ru.netology.FormDate;
+import ru.netology.Post;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Post post = new Post();
+        post.name = "Ivan";
+        post.surname = "John";
+        post.passport = "4444№4444444";
+        post.patronymic = "Conner";
+        post.birthday = new FormDate();
+        post.birthday.day = "3";
+        post.birthday.month = "12";
+        post.birthday.year = "1987";
+        post.phone = "+7(999)999 99 99";
+        post.subscription = true;
     }
 }
---
+++ src/main/java/ru/netology/Post.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Post {
+    public String name;
+    public String surname;
+    public FormDate birthday;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public boolean subscription;
+}
