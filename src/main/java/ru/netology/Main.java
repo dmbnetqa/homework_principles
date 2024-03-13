721d1d3647befdad0236e9addf281f1ae25b1f2c
Дамба Манджиев <mdamba2020@yandex.ru>
2024-03-13 07:17:17 +0300

    change data type into exercise_1

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -9,9 +9,9 @@ public class Main {
         post.passport = "4444№4444444";
         post.patronymic = "Conner";
         post.birthday = new FormDate();
-        post.birthday.day = "3";
-        post.birthday.month = "12";
-        post.birthday.year = "1987";
+        post.birthday.day = 3;
+        post.birthday.month = 12;
+        post.birthday.year = 1987;
         post.phone = "+7(999)999 99 99";
         post.subscription = true;
     }
